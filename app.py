ba56a9cc55db722a0f15e7575a34eabaad1b19e5
kehindeadedara <35585101+kehindeadedara@users.noreply.github.com>
2020-11-22 10:17:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,33 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+import random
+import numpy as np
 
-data = []
+def random():
+    randomlist = []
+    for i in range(0, 10):
+        n = np.random.randint(1, 30)
+        randomlist.append(n)
+    return randomlist
+
+data = [50,10,20,30,40]
 class Shopvision(Resource):
-    def post(self, newdata):
-        occupant = {'occupancy': str(newdata)}
-        data.append(occupant)
+
+    def put(self, newdata):
+        # newdata = {'name': newdata}
+        # data.append(newdata)
         return newdata
 
     def get(self):
-        return {'data': data}
+        return {'data': random()}
 
 api.add_resource(Shopvision, '/')
 
 @app.route("/")
 def hello():
     return "Shop Vision API"
+
+
+if __name__ == "__main__":
+    app.run(debug= True);
