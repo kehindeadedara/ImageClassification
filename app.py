37ce961c9351c9feb99edb1125704f0effdb0b01
kehindeadedara <35585101+kehindeadedara@users.noreply.github.com>
2020-11-22 09:07:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from flask import Flask
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
+
+data = []
+class Shopvision(Resource):
+    def post(self, newdata):
+        occupant = {'occupancy': str(newdata)}
+        data.append(occupant)
+        return newdata
+
+    def get(self):
+        return {'data': data}
+
+api.add_resource(Shopvision, '/')
 
 @app.route("/")
 def hello():
